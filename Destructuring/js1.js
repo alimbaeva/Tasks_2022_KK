ba577062b3e70bcaee56b890002e97b94d1cd468
Asel <asel.alimbaeva.kk@gmail.com>
2022-03-31 19:06:52 +0300

    fix: add files js

---
+++ Destructuring/js1.js
@@ -0,0 +1,17 @@
+
+// =============
+
+console.log('2-задача');
+
+function func() {
+    return ['Иван', 'Иванов', 'отдел разработки', 'программист', 2000];
+}
+
+let [name1, surname1, department1, position1, salary1] = func()
+
+console.log('name ', name1)
+console.log('surname ', surname1)
+console.log('department', department1)
+console.log('position ', position1)
+console.log('salary ', salary1)
+
---
+++ Destructuring/js3.js
@@ -0,0 +1,13 @@
+
+// ==============
+
+console.log('4-задача');
+
+let arr4 = ['Иван', 'Иванов', 'отдел разработки', 'программист', 2000];
+
+let [name3, surname3, ...salary3] = arr4
+
+console.log('name3', name3)
+console.log('surname3 ', surname3)
+console.log('salary3 ', salary3)
+
